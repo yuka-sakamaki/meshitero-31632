322340bc6758f23159e6f151b856b602ecc54e82
super-sausage <69895997+super-sausage@users.noreply.github.com>
2020-11-26 18:08:57 +0900

    deviseの導入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private 
+     def configure_permitted_parameters
+       devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :last_name, :first_name, :prefecture_code_id])
+     end
 end
--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,5 @@
 class TweetsController < ApplicationController
+  before_action :move_to_index, except: [:index]
 
   def index
     @tweet = Tweet.order('created_at DESC')
@@ -22,4 +23,10 @@ class TweetsController < ApplicationController
     params.require(:tweet).permit(:text, images:[]).merge(user_id: current_user.id)
   end
 
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
+  end
+  
 end
