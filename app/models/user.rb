6dcacd05bef9365cdc786e7500f58332e8f2d758
super-sausage <69895997+super-sausage@users.noreply.github.com>
2020-11-26 18:54:49 +0900

    ユーザー新規登録バリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  has_many :tweets
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :prefecture_code
+
+  with_options presence: true do
+    validates :nickname
+    validates :last_name, :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
+    validates :prefecture_code
+  end
 end
